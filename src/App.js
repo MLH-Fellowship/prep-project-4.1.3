816ecc4f0db73f8b674107f030e72f18fd8ffc43
sumana2001 <sumana.basu2001@gmail.com>
2021-11-15 21:45:41 +0530

    Feat: Geolocation API fetched and result displayed

--- src/App.js
+++ src/App.js
@@ -1,67 +1,93 @@
-import { useEffect, useState } from 'react';
-import logo from './assets/img/mlh-prep.png';
+import { useEffect, useState } from "react";
+import logo from "./assets/img/mlh-prep.png";
 
 const App = () => {
-	const [error, setError] = useState(null);
-	const [isLoaded, setIsLoaded] = useState(false);
-	const [city, setCity] = useState('New York City');
-	const [results, setResults] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [city, setCity] = useState("");
+  const [results, setResults] = useState(null);
 
-	useEffect(() => {
-		fetch(
-			'https://api.openweathermap.org/data/2.5/weather?q=' +
-				city +
-				'&units=metric' +
-				'&appid=' +
-				process.env.REACT_APP_APIKEY
-		)
-			.then((res) => res.json())
-			.then(
-				(result) => {
-					if (result['cod'] !== 200) {
-						setIsLoaded(false);
-					} else {
-						setIsLoaded(true);
-						setResults(result);
-					}
-				},
-				(error) => {
-					setIsLoaded(true);
-					setError(error);
-				}
-			);
-	}, [city]);
+  useEffect(() => {
+    function onSuccess(position) {
+      let latitude = position.coords.latitude;
+      let longitude = position.coords.longitude;
 
-	if (error) return <div>Error: {error.message}</div>;
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_APIKEY}`
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setCity(result.name);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
+    }
+    function onError(error) {
+      console.log(error);
+      setCity("New York City");
+    }
 
-	return (
-		<>
-			<img className='logo' src={logo} alt='MLH Prep Logo'></img>
-			<div>
-				<h2>Enter a city below 👇</h2>
-				<input
-					type='text'
-					value={city}
-					onChange={(event) => setCity(event.target.value)}
-				/>
-				<div className='Results'>
-					{!isLoaded && <h2>Loading...</h2>}
-					{console.log(results)}
-					{isLoaded && results && (
-						<>
-							<h3>{results.weather[0].main}</h3>
-							<p>Feels like {results.main.feels_like}°C</p>
-							<i>
-								<p>
-									{results.name}, {results.sys.country}
-								</p>
-							</i>
-						</>
-					)}
-				</div>
-			</div>
-		</>
-	);
+    window.navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    if (city !== "") {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric" +
+          "&appid=" +
+          process.env.REACT_APP_APIKEY
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            if (result["cod"] !== 200) {
+              setIsLoaded(false);
+            } else {
+              setIsLoaded(true);
+              setResults(result);
+            }
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
+    }
+  }, [city]);
+
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <>
+      <img className="logo" src={logo} alt="MLH Prep Logo"></img>
+      <div>
+        <h2>Enter a city below 👇</h2>
+        <input
+          type="text"
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+        />
+        <div className="Results">
+          {!isLoaded && <h2>Loading...</h2>}
+          {console.log(results)}
+          {isLoaded && results && (
+            <>
+              <h3>{results.weather[0].main}</h3>
+              <p>Feels like {results.main.feels_like}°C</p>
+              <i>
+                <p>
+                  {results.name}, {results.sys.country}
+                </p>
+              </i>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;

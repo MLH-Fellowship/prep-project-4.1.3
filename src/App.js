70df009a50d492f4328f3efaf3d28021d7712041
sumana2001 <sumana.basu2001@gmail.com>
2021-11-15 21:57:53 +0530

    Feat: Toast added and useEffect fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import logo from "./assets/img/mlh-prep.png";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   const [error, setError] = useState(null);
@@ -7,31 +9,49 @@ const App = () => {
   const [city, setCity] = useState("");
   const [results, setResults] = useState(null);
 
+  useEffect(()=>{
+	const options = {
+		enableHighAccuracy: false,
+		timeout: 5000,
+		maximumAge: Infinity,
+	  };
+  
+	  function onSuccess(position) {
+		let latitude = position.coords.latitude;
+		let longitude = position.coords.longitude;
+  
+		fetch(
+		  `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_APIKEY}`
+		)
+		  .then((res) => res.json())
+		  .then(
+			(result) => {
+			  setCity(result.name);
+			},
+			(error) => {
+			  setIsLoaded(true);
+			  setError(error);
+			}
+		  );
+	  }
+  
+	  function onError(error) {
+		  toast.configure();
+		  toast.error("Location access denied.", {
+		  theme: "light",
+		  hideProgressBar: false,
+		  closeButton: true,
+		  position: "top-right",
+		  autoClose: 3000,
+		});
+		setCity("New York City");
+	  }
+  
+	  window.navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+	  
+  },[])
+
   useEffect(() => {
-    function onSuccess(position) {
-      let latitude = position.coords.latitude;
-      let longitude = position.coords.longitude;
-
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_APIKEY}`
-      )
-        .then((res) => res.json())
-        .then(
-          (result) => {
-            setCity(result.name);
-          },
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        );
-    }
-    function onError(error) {
-      console.log(error);
-      setCity("New York City");
-    }
-
-    window.navigator.geolocation.getCurrentPosition(onSuccess, onError);
     if (city !== "") {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +

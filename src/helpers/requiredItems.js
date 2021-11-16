412390b172273df0634363a6b9758a9736f75327
OjusWiZard <ojuswimail@gmail.com>
2021-11-16 19:45:56 +0530

    added some more items in the required conditions
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- src/helpers/requiredItems.js
+++ src/helpers/requiredItems.js
@@ -10,6 +10,9 @@ import Goggles from '../assets/img/Goggles.png';
 import SandScarf from '../assets/img/Scarf.png';
 import Flashlight from '../assets/img/Flashlight.png';
 import Jacket from '../assets/img/Jacket.png';
+import Watch from '../assets/img/Watch.png';
+import Sunscream from '../assets/img/Sunscream.png';
+import MosquitoRepellent from '../assets/img/MosquitoRepellent.png';
 
 const requiredItems = {
 	Thunderstorm: {
@@ -32,35 +35,61 @@ const requiredItems = {
 		Gloves: Gloves,
 		Jacket: Jacket,
 	},
-	Mist: {},
+	Mist: {
+		Watch: Watch,
+		Goggles: Goggles,
+		Mask: Mask,
+	},
 	Smoke: {
+		Watch: Watch,
+		Goggles: Goggles,
 		Mask: Mask,
 	},
 	Haze: {
+		Watch: Watch,
+		Goggles: Goggles,
 		Mask: Mask,
 	},
 	Dust: {
+		Watch: Watch,
+		Goggles: Goggles,
 		Mask: Mask,
 	},
-	Fog: {},
+	Fog: {
+		Watch: Watch,
+		Goggles: Goggles,
+		Mask: Mask,
+	},
 	Sand: {
 		Mask: Mask,
 		Goggles: Goggles,
 		'Sand Scarf': SandScarf,
 	},
 	Ash: {
+		Watch: Watch,
+		Goggles: Goggles,
 		Mask: Mask,
 	},
-	Squall: {},
+	Squall: {
+		Watch: Watch,
+		Goggles: Goggles,
+		Mask: Mask,
+	},
 	Tornado: {
 		Sanitation: Sanitation,
 		Flashlight: Flashlight,
+		Goggles: Goggles,
 	},
 	Clear: {
 		Sunglasses: Sunglasses,
 		BaseballCap: Cap,
+		Sunscream: Sunscream,
 	},
-	Clouds: {},
+	Clouds: {
+		BaseballCap: Cap,
+		Watch: Watch,
+		'Mosquito Repellent': MosquitoRepellent,
+	},
 };
 
 export default requiredItems;

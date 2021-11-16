04289f1f4a67f37e1940156d00be3ae91cfac089
OjusWiZard <ojuswimail@gmail.com>
2021-11-16 18:39:18 +0530

    fixed css and items count
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- src/helpers/requiredItems.js
+++ src/helpers/requiredItems.js
@@ -1,14 +1,10 @@
 import Raincoat from '../assets/img/Raincoat.png';
-import Towel from '../assets/img/Towel.png';
 import Sanitation from '../assets/img/Sanitation.png';
-import WaterResistantBagpack from '../assets/img/WaterResistantBagpack.png';
 import WaterproofBoots from '../assets/img/WaterproofBoots.png';
 import Umbrella from '../assets/img/Umbrella.png';
-import Scarf from '../assets/img/Scarf.png';
 import Cap from '../assets/img/Cap.png';
 import Sunglasses from '../assets/img/Sunglasses.png';
 import Gloves from '../assets/img/Gloves.png';
-import Socks from '../assets/img/Socks.png';
 import Mask from '../assets/img/Mask.png';
 import Goggles from '../assets/img/Goggles.png';
 import SandScarf from '../assets/img/Scarf.png';
@@ -18,31 +14,22 @@ import Jacket from '../assets/img/Jacket.png';
 const requiredItems = {
 	Thunderstorm: {
 		Raincoat: Raincoat,
-		Towel: Towel,
 		Flashlight: Flashlight,
-		Sanitation: Sanitation,
-		'Water Resistant Bagpack': WaterResistantBagpack,
 		'Waterproof Boots': WaterproofBoots,
 	},
 	Drizzle: {
 		Raincoat: Raincoat,
-		Towel: Towel,
 		'Waterproof Boots': WaterproofBoots,
 		Umbrella: Umbrella,
 	},
 	Rain: {
 		Raincoat: Raincoat,
-		Towel: Towel,
-		'Water Resistant Bagpack': WaterResistantBagpack,
 		'Waterproof Boots': WaterproofBoots,
 		Umbrella: Umbrella,
 	},
 	Snow: {
-		Scarf: Scarf,
-		Cap: Cap,
 		Sunglasses: Sunglasses,
 		Gloves: Gloves,
-		Socks: Socks,
 		Jacket: Jacket,
 	},
 	Mist: {},
@@ -69,7 +56,10 @@ const requiredItems = {
 		Sanitation: Sanitation,
 		Flashlight: Flashlight,
 	},
-	Clear: {},
+	Clear: {
+		Sunglasses: Sunglasses,
+		BaseballCap: Cap,
+	},
 	Clouds: {},
 };
 

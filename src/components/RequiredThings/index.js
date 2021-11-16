6db7262108155c8a72e787d0277005a0b19de401
PriyaBihani <priyabihani.official@gmail.com>
2021-11-16 19:58:05 +0530

    restructuring required things component

--- src/components/RequiredThings/index.js
+++ src/components/RequiredThings/index.js
@@ -9,29 +9,31 @@ const RequiredThings = (props) => {
 	}, [props.results.weather[0].main]);
 
 	return (
-		<>
+		<div className='required-things-container'>
 			{Object.keys(items).length > 0 && (
-				<h1>Don't forget to bring your</h1>
-			)}
-			<div className="required-things">
-				{Object.keys(items).map((item, i) => (
-					<div className="card-item">
-						<div className="face face1">
-							<div className="content">
-								<div className="icon">
-									<img src={items[item]} />
+				<>
+					<h1>Don't forget to bring your</h1>
+					<div className='required-things'>
+						{Object.keys(items).map((item, i) => (
+							<div className='card-item'>
+								<div className='face face1'>
+									<div className='content'>
+										<div className='icon'>
+											<img src={items[item]} />
+										</div>
+									</div>
+								</div>
+								<div className='face face2'>
+									<div className='content'>
+										<h3>{item}</h3>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div className="face face2">
-							<div className="content">
-								<h3>{item}</h3>
-							</div>
-						</div>
+						))}
 					</div>
-				))}
-			</div>
-		</>
+				</>
+			)}
+		</div>
 	);
 };
 

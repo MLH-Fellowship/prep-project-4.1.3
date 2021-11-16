71c2602b363f7f6e7697383502965157f79a1074
PriyaBihani <priyabihani.official@gmail.com>
2021-11-16 11:35:05 +0530

    Added required items

---
+++ src/helpers/requiredItems.js
@@ -0,0 +1,19 @@
+const requiredItem = {
+	Thunderstorm: ['Raincoat', 'Flashlight', 'Watch'],
+	Drizzle: ['Raincoat', 'Flashlight', 'Watch'],
+	Rain: ['Raincoat', 'Flashlight', 'Watch'],
+	Snow: ['Raincoat', 'Flashlight', 'Watch'],
+	Mist: ['Raincoat', 'Flashlight', 'Watch'],
+	Smoke: ['Raincoat', 'Flashlight', 'Watch'],
+	Haze: ['Raincoat', 'Flashlight', 'Watch'],
+	Dust: ['Raincoat', 'Flashlight', 'Watch'],
+	Fog: ['Raincoat', 'Flashlight', 'Watch'],
+	Sand: ['Raincoat', 'Flashlight', 'Watch'],
+	Ash: ['Raincoat', 'Flashlight', 'Watch'],
+	Squall: ['Raincoat', 'Flashlight', 'Watch'],
+	Tornado: ['Raincoat', 'Flashlight', 'Watch'],
+	Clear: ['Raincoat', 'Flashlight', 'Watch'],
+	Clouds: ['Raincoat', 'Flashlight', 'Watch'],
+};
+
+export default requiredItem;

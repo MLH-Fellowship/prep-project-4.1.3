6999a7fd14f00ed624ea9a283add2206f79b1eda
OjusWiZard <ojuswimail@gmail.com>
2021-11-19 02:18:57 +0530

    fixed loading at empty search string
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- src/helpers/customHooks/useWeather.js
+++ src/helpers/customHooks/useWeather.js
@@ -60,9 +60,9 @@ const useWeather = () => {
   }, []);
 
   useEffect(() => {
-    setIsLoading(true);
     setIsLoaded(false);
     if (city !== "") {
+      setIsLoading(true);
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
           city +

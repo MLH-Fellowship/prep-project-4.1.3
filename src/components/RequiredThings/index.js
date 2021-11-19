c2f350a0eda933e4746152981b142033125ed576
Neha Kumari <69652168+neha-93@users.noreply.github.com>
2021-11-19 10:26:31 +0530

    Revert "resolving some merge conflicts"

--- src/components/RequiredThings/index.js
+++ src/components/RequiredThings/index.js
@@ -1,40 +1,40 @@
-import React, { useState, useEffect } from "react";
-import requiredItems from "../../helpers/requiredItems";
+import React, { useState, useEffect } from 'react';
+import requiredItems from '../../helpers/requiredItems';
 
 const RequiredThings = (props) => {
-  const [items, setItems] = useState([]);
+	const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    setItems(requiredItems[props.results.weather[0].main]);
-  }, [props.results.weather]);
+	useEffect(() => {
+		setItems(requiredItems[props.results.weather[0].main]);
+	}, [props.results.weather[0].main]);
 
-  return (
-    <div className="required-things-container">
-      {Object.keys(items).length > 0 && (
-        <>
-          <h1>Don't forget to bring your</h1>
-          <div className="required-things">
-            {Object.keys(items).map((item, i) => (
-              <div className="card-item">
-                <div className="face face1">
-                  <div className="content">
-                    <div className="icon">
-                      <img src={items[item]} />
-                    </div>
-                  </div>
-                </div>
-                <div className="face face2">
-                  <div className="content">
-                    <h3>{item}</h3>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className='required-things-container'>
+			{Object.keys(items).length > 0 && (
+				<>
+					<h1>Don't forget to bring your</h1>
+					<div className='required-things'>
+						{Object.keys(items).map((item, i) => (
+							<div className='card-item'>
+								<div className='face face1'>
+									<div className='content'>
+										<div className='icon'>
+											<img src={items[item]} />
+										</div>
+									</div>
+								</div>
+								<div className='face face2'>
+									<div className='content'>
+										<h3>{item}</h3>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default RequiredThings;

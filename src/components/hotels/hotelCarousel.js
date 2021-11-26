ee7938b3795b30ede63f77db7eb3453959b5957d
neha_93 <nkrocks93@gmail.com>
2021-11-26 19:41:56 +0530

    added styles to trip-planner page

--- src/components/hotels/hotelCarousel.js
+++ src/components/hotels/hotelCarousel.js
@@ -1,10 +1,38 @@
 import HotelCard from "./hotelCard";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleLeft, faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
+import "./hotelCarousel.css";
 
-const HotelCarousel = ({hotels}) => {
-  
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    >
+      <FontAwesomeIcon icon={faChevronCircleLeft} color="#000" size="3x" />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    >
+      <FontAwesomeIcon icon={faChevronCircleRight} color="#000" size="3x" />
+    </div>
+  );
+}
+
+const HotelCarousel = ({ hotels }) => {
   var settings = {
     dots: true,
     infinite: false,
@@ -12,6 +40,9 @@ const HotelCarousel = ({hotels}) => {
     slidesToShow: 4,
     slidesToScroll: 4,
     initialSlide: 0,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    className: 'slides',
     responsive: [
       {
         breakpoint: 1024,
@@ -19,34 +50,33 @@ const HotelCarousel = ({hotels}) => {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <div>
       <Slider {...settings}>
-        {hotels.map((hotel,index) => (
-          <HotelCard hotel={hotel}/>
-        )
-        )}
+        {hotels.map((hotel, index) => (
+          <HotelCard hotel={hotel} />
+        ))}
       </Slider>
     </div>
   );
